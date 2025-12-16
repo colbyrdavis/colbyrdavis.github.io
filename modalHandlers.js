87be0dd6e40f2366afb9c92c8f8a18bfb9a20c4d
colbyrdavis <134110610+colbyrdavis@users.noreply.github.com>
2025-12-16 00:09:10 -0500

    Add files via upload

--- modalHandlers.js
+++ modalHandlers.js
@@ -1,6 +1,1 @@
-const openMissilePN = document.getElementById("openMissilePN");
-const missilePN = document.getElementById("missilePN");
-const closeMissilePN = document.getElementById("closeMissilePN");
-
-openMissilePN.onclick = () => missilePN.classList.remove("hidden");
-closeMissilePN.onclick = () => missilePN.classList.add("hidden");
+/* all modal open/close handlers unchanged plus missile PN */
